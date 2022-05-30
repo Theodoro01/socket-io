3a4f89663b4b66f54fb2eb38af8cfd26257f78a3
Theodoro01 <theodorovieira01@gmail.com>
2022-05-30 16:31:10 -0300

    socket-io

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+var http = require("http").createServer(app);
+var io = require("socket.io")(http);
+
+io.on('connection', (socket) => {
+
+    socket.on("disconect", () => {
+        console.log("Usuario " + socket.id + " está offline.");
+    });
+
+    socket.on("msg", (data) => {
+        socket.emit("showmsg", data);
+        console.log(data);
+    });
+
+});
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => { 
+    res.render("index");
+});
+
+http.listen(3000, (req, res) => {
+    console.log("Hello World!");
+});
